3d36874a9f1682fea2d78f11eead120d9ea74fa6
Alexandru Moșoi <mosoi@google.com>
2015-08-14 10:35:17 +0200

    driver: improve script.

--- driver/driver.sh
+++ driver/driver.sh
@@ -2,22 +2,37 @@
 
 while `true`; do
   seed=$RANDOM$RANDOM
-  echo $seed
-  generate -dir ./ -seed $seed -out a.go
+  generate -dir ./ -seed $seed -out a$$.go
 
-  GOROOT=$HOME/go1.4 $HOME/go1.4/bin/go run a.go &> good
+  GOROOT=$HOME/go1.4 $HOME/go1.4/bin/go build a$$.go 2> /dev/null
   if [ $? -ne 0 ]; then
-    echo =================================
-    cat a.go
-    echo ---------------------------------
-    cat good
-    echo =================================
+    #echo =================================
+    #cat a.go
+    #echo =================================
     continue
   fi
 
-  GOROOT=$HOME/go $HOME/go/bin/go run a.go &> bad
-  diff -q -w good bad
+  ./a$$ &> good$$
+
+  GOROOT=$HOME/go $HOME/go/bin/go build a$$.go 2> log$$
   if [ $? -ne 0 ]; then
-    exit 1
+    echo broken $seed
+    tail -n1 log$$
+
+    printf "// " > broken-$seed.go
+    tail -n1 log$$ >> broken-$seed.go
+    cat a$$.go >> broken-$seed.go
+    continue
+  fi
+  ./a$$ &> bad$$
+
+  diff -q -w good$$ bad$$
+  if [ $? -ne 0 ]; then
+    echo failed $seed
+    print good `cat good$$`
+    print bad `cat bad$$`
+
+    cp a$$.go failed-$seed.go
+    continue
   fi
 done
