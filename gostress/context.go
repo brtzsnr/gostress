ee9966d1572d787b5bf88fed3849e2aeeb58c07e
Alexandru Moșoi <mosoi@google.com>
2015-09-02 19:21:19 +0200

    context: Refactor a bit. Temporarity drop bool.

--- gostress/context.go
+++ gostress/context.go
@@ -6,14 +6,45 @@ import (
 	"math/rand"
 )
 
-var (
-	basicTypes = []string{"bool", "int", "uint", "int8", "uint8", "int64", "uint64"}
-	idSeq      = make(map[string]int)
-
-	exprDepth     = 5
+const (
+	exprDepth     = 4
 	numVariables  = 5
-	numStatements = 10
-	numArgs       = 5
+	numStatements = 5
+	numArgs       = 1
+)
+
+var (
+	// basic expression types. - is untyped integer.
+	basicTypes = []string{ /*"bool",*/ "int", "uint", "int8", "uint8", "int64", "uint64"}
+	// maps prefix to next unused ids
+	idSeq = make(map[string]int)
+	// operator precendence
+	precedence = map[string]int{
+		"*":  5,
+		"/":  5,
+		"%":  5,
+		"<<": 5,
+		">>": 5,
+		"&":  5,
+		"&^": 5,
+		"+":  4,
+		"-":  4,
+		"|":  4,
+		"^":  4,
+		"==": 3,
+		"!=": 3,
+		"<":  3,
+		"<=": 3,
+		">":  3,
+		">=": 3,
+		"&&": 2,
+		"||": 1,
+
+		"()":    0,
+		"call":  0,
+		"conv":  0,
+		"token": 0,
+	}
 )
 
 // fnct represents a function.
@@ -21,25 +52,71 @@ type fnct struct {
 	typ string // returning type
 	nam string // name of the function
 
-	arg []vrbl
-	lit []vrbl // a list of live literals
-	stm []stmt // a list of statements
+	arg []*expr // a list of arguments
+	lit []*expr // a list of live literals
+	stm []*stmt // a list of statements
 }
 
 type expr struct {
-	typ string // expression type
-	str string // string
-	atm bool   // if it's atom
+	typ      string // expression type
+	opr      string // operation, e.g. &&, +
+	tok      string // token if it's not an expression
+	lft, rgt *expr  // operands. right is nil for unary
 }
 
-type vrbl struct { // a variable
-	typ string // variable's type
-	nam string // variable's name.
+func (e *expr) String() string {
+	switch e.opr {
+	case "()":
+		return fmt.Sprintf("(%s)", e.lft)
+	case "call":
+		// HACK because argument list is generated early.
+		return e.tok
+	case "conv":
+		return fmt.Sprintf("%s(%s)", e.typ, e.lft)
+	case "token":
+		return e.tok
+	default:
+		return fmt.Sprintf("%s %s %s", e.lft, e.opr, e.rgt)
+	}
+}
+
+// prec makes e with precence at least as op.
+func prec(e *expr, op string) *expr {
+	if precedence[e.opr] <= precedence[op] {
+		return e
+	}
+	return &expr{
+		typ: e.typ,
+		opr: "()",
+		lft: e,
+	}
+}
+
+// conv returns an expression that converts e to typ.
+func conv(e *expr, typ string) *expr {
+	if typ == e.typ { // already of correct type
+		return prec(e, "conv")
+	}
+
+	return &expr{
+		typ: typ,
+		opr: "conv",
+		lft: e,
+	}
+}
+
+// constant returns a constant of type typ.
+func constant(typ string) *expr {
+	return &expr{
+		typ: "-",
+		opr: "token",
+		tok: fmt.Sprint(rnd(4)),
+	}
 }
 
 type stmt struct {
 	opr string // operation, e.g :=, _
-	lit *vrbl  // literal
+	lit *expr  // literal
 	exp *expr  // expression
 }
 
@@ -53,30 +130,31 @@ func newFnct(typ string) *fnct {
 		typ: typ,
 	}
 
-	argc := rnd(numArgs+1)
+	argc := rnd(numArgs + 1)
 	for i := 0; i < argc; i++ {
-		f.arg = append(f.arg, vrbl{
-			nam: newId("a"),
+		f.arg = append(f.arg, &expr{
 			typ: choice(basicTypes...),
+			opr: "token",
+			tok: newId("a"),
 		})
 	}
 
 	for i := 0; i < numStatements; i++ {
 		switch o := choice(":=", "=", "+=", "-=", "*=", "<<=", ">>=", "++", "--"); o {
 		case "return":
-			f.stm = append(f.stm, stmt{
+			f.stm = append(f.stm, &stmt{
 				opr: "return",
 				exp: f.newExpr(typ, 0),
 			})
 		case ":=":
 			l := f.newVariable(choice(basicTypes...))
-			f.stm = append(f.stm, stmt{
+			f.stm = append(f.stm, &stmt{
 				opr: "_",
 				lit: l,
 			})
 		case "=", "+=", "-=", "*=":
 			if l := f.getVariable(""); l != nil {
-				f.stm = append(f.stm, stmt{
+				f.stm = append(f.stm, &stmt{
 					opr: o,
 					lit: l,
 					exp: f.newExpr(l.typ, 0),
@@ -84,7 +162,7 @@ func newFnct(typ string) *fnct {
 			}
 		case "<<=", ">>=":
 			if l := f.getVariable(""); l != nil {
-				f.stm = append(f.stm, stmt{
+				f.stm = append(f.stm, &stmt{
 					opr: o,
 					lit: l,
 					exp: f.newExpr("uint", 0),
@@ -92,7 +170,7 @@ func newFnct(typ string) *fnct {
 			}
 		case "++", "--":
 			if l := f.getVariable(""); l != nil {
-				f.stm = append(f.stm, stmt{
+				f.stm = append(f.stm, &stmt{
 					opr: o,
 					lit: l,
 				})
@@ -100,7 +178,7 @@ func newFnct(typ string) *fnct {
 		}
 	}
 
-	f.stm = append(f.stm, stmt{
+	f.stm = append(f.stm, &stmt{
 		opr: "return",
 		exp: f.newExpr(typ, 0),
 	})
@@ -110,7 +188,7 @@ func newFnct(typ string) *fnct {
 func (f *fnct) dump(w io.Writer) {
 	fmt.Fprintf(w, "func %s(", f.nam)
 	for _, a := range f.arg {
-		fmt.Fprintf(w, "%s %s, ", a.nam, a.typ)
+		fmt.Fprintf(w, "%s %s, ", a.tok, a.typ)
 	}
 	fmt.Fprintf(w, ") %s {\n", f.typ)
 	fmt.Fprintf(w, "switch {} // prevent inlining\n")
@@ -120,137 +198,78 @@ func (f *fnct) dump(w io.Writer) {
 	fmt.Fprintf(w, "}\n")
 }
 
-func (f *fnct) newVariable(typ string) *vrbl {
-	// placeholder.
-	var nl, ns int
-	nl, f.lit = len(f.lit), append(f.lit, vrbl{})
+// newVariable creates a new variable.
+func (f *fnct) newVariable(typ string) *expr {
 	e := f.newExpr(typ, 0)
-	ns, f.stm = len(f.stm), append(f.stm, stmt{})
-
 	if typ == "-" {
 		typ = "int"
 	}
-	f.lit[nl] = vrbl{
+	lit := &expr{
 		typ: typ,
-		nam: newId("v"),
+		opr: "token",
+		tok: newId("v"),
 	}
-	f.stm[ns] = stmt{
+	stm := &stmt{
 		opr: ":=",
-		lit: &f.lit[nl],
+		lit: lit,
 		exp: e,
 	}
-	return &f.lit[nl]
-}
-
-func atom(e *expr) *expr {
-	if e.atm {
-		return e
-	}
-		return &expr{
-			typ: e.typ,
-			str: fmt.Sprintf("(%s)", e.str),
-			atm: true,
-		}
-}
-
-// conv returns an expression that converts e to typ.
-func conv(e *expr, typ string) *expr {
-	if typ == e.typ { // already of correct type
-		return atom(e)
-	}
-
-	if typ == "bool" {
-		return &expr{
-			typ: typ,
-			str: fmt.Sprintf("(%s > 0)", atom(e).str),
-			atm: true,
-		}
-	}
-	if e.typ == "bool" {
-		return &expr{
-			typ: typ,
-			str: fmt.Sprintf("map[bool]%s{false: %s, true: %s}[%s]", typ, conv(constant(), typ).str, conv(constant(), typ).str, e.str),
-			atm: true,
-		}
-	}
-	return &expr{
-		typ: typ,
-		str: fmt.Sprintf("%s(%s)", typ, e.str),
-		atm: true,
-	}
+	fmt.Printf("lit %#v\n", lit)
+	fmt.Printf("stm %#v\n", stm.exp)
+	f.lit = append(f.lit, lit)
+	f.stm = append(f.stm, stm)
+	return lit
 }
 
 // getVariable returns a random live variable. Returns nil if none is found.
 // typ == "" means any type.
-func (f *fnct) getVariable(typ string) *vrbl {
+func (f *fnct) getVariable(typ string) *expr {
 	if rnd(10) == 0 {
 		return nil
 	}
 	if rnd(2) == 0 && len(f.lit) > 0 {
-		l := &f.lit[rnd(len(f.lit))]
+		// Pick an already defined literal.
+		l := f.lit[rnd(len(f.lit))]
 		if typ == "" || l.typ == typ {
 			return l
 		}
 	} else if len(f.arg) > 0 {
-		l := &f.arg[rnd(len(f.arg))]
+		// Pick an argument.
+		l := f.arg[rnd(len(f.arg))]
 		if typ == "" || l.typ == typ {
 			return l
 		}
 	}
+	// Retry.
 	return f.getVariable(typ)
 }
 
 // getLiteral returns a constant or a variable.
 func (f *fnct) getLiteral(typ string) *expr {
-	if typ == "bool" {
-		if rnd(2) == 0 {
-			return &expr {
-				typ: "bool",
-				str: "true",
-				atm: true,
-			}
-		} else {
-			return &expr {
-				typ: "bool",
-				str: "false",
-				atm: true,
-			}
-		}
-	}
 	if rnd(25) == 0 { // don't generate many constants
-		return constant()
+		return constant(typ)
 	}
 	if l := f.getVariable(typ); l != nil {
-		return conv(&expr{
-			typ: l.typ,
-			str: l.nam,
-			atm: true,
-		}, typ)
+		return conv(l, typ)
 	}
 	return f.getLiteral(typ)
 }
 
+// call returns an expression to call a function.
 func (f *fnct) call(o *fnct) *expr {
 	str := o.nam + "("
-	for range o.arg {
-		str += fmt.Sprintf("%d, ", rnd(10))
+	for _, a := range o.arg {
+		// HACK because we store the whole function call in tok.
+		str += fmt.Sprintf("%s, ", constant(a.typ).tok)
 	}
 	str += ")"
 
 	return &expr{
 		typ: o.typ,
-		str: str,
+		opr: "call",
+		tok: str,
 	}
 }
-
-func constant() *expr {
-	return &expr{
-		typ: "-",
-		str: fmt.Sprintf("%d", rnd(4)),
-		atm: true,
-	}
-}
-
 
 func (f *fnct) newExpr(typ string, dep int) *expr {
 	if rnd(exprDepth) == 0 || dep > exprDepth {
@@ -261,7 +280,7 @@ func (f *fnct) newExpr(typ string, dep int) *expr {
 	if typ == "bool" {
 		ops = []string{"&&", "||", "!=", "==", "conv"}
 	} else {
-		ops = []string{"+", "-", "*", /*"/", "%", */ "&", "|", "^", "()", "<<", ">>", "++", "--", "conv"}
+		ops = []string{"+", "-", "*" /*"/", "%", */, "&", "|", "^", "&^", "<<", ">>", "++", "--", "conv"}
 	}
 
 retry:
@@ -269,67 +288,36 @@ retry:
 	case "&&", "||":
 		l := f.newExpr(typ, dep+1)
 		r := f.newExpr(typ, dep+1)
-		return &expr{
-			typ: typ,
-			str: fmt.Sprintf("%s %s %s", l.str, op, r.str),
-		}
+		l, r = prec(l, op), prec(r, op)
+		return &expr{typ: typ, opr: op, lft: l, rgt: r}
 
 	case "!=", "==":
 		ntyp := choice(basicTypes...)
 		l := f.newExpr(ntyp, dep+1)
 		r := f.newExpr(ntyp, dep+1)
-		return &expr{
-			typ: typ,
-			str: fmt.Sprintf("%s %s %s", l.str, op, r.str),
-		}
-	
+		l, r = prec(l, op), prec(r, op)
+		return &expr{typ: typ, opr: op, lft: l, rgt: r}
+
 	case "+", "-", "*", "/", "%", "&", "|", "^":
 		l := f.newExpr(typ, dep+1)
 		r := f.newExpr(typ, dep+1)
+		l, r = prec(l, op), prec(r, op)
 		if l.typ == "-" && r.typ == "-" {
 			typ = "-"
 		}
-
-		if op != "/" && op != "%" {
-			return &expr{
-				typ: typ,
-				str: fmt.Sprintf("%s %s %s", l.str, op, r.str),
-			}
-		} else {
-			return &expr{
-				typ: typ,
-				str: fmt.Sprintf("%s %s (1|(%s))", l.str, op, r.str),
-			}
-		}
+		return &expr{typ: typ, opr: op, lft: l, rgt: r}
 	case ">>", "<<":
-		l := conv(f.newExpr(typ, dep+1), typ)
+		l := f.newExpr(typ, dep+1)
 		r := f.newExpr(choice("-", "uint", "uint8", "uint16", "uint32", "uint64"), dep+1)
-
-		if !r.atm {
-			r.str = "(" + r.str + ")"
-			r.atm = true
+		l, r = prec(l, op), prec(r, op)
+		if l.typ == "-" {
+			l = conv(l, typ)
 		}
-
-		return &expr{
-			typ: typ,
-			str: fmt.Sprintf("%s %s %s", l.str, op, r.str),
-		}
-	case "()":
-		e := f.newExpr(typ, dep+1)
-		if e.atm {
-			return e
-		}
-		if e.typ == "-" {
-			typ = "-"
-		}
-		return &expr{
-			typ: typ,
-			str: fmt.Sprintf("(%s)", e.str),
-			atm: true,
-		}
+		return &expr{typ: typ, opr: op, lft: l, rgt: r}
 	case "conv":
 		if typ == "-" || rnd(5) != 0 {
 			// Cannot convert to untyped.
+			// Also, don't convert very often.
 			break
 		}
 		return conv(f.newExpr(choice(basicTypes...), dep+1), typ)
@@ -340,17 +328,17 @@ retry:
 func (s *stmt) dump(w io.Writer) {
 	switch s.opr {
 	case "return":
-		fmt.Fprintf(w, "return %s\n", s.exp.str)
+		fmt.Fprintf(w, "return %s\n", s.exp)
 	case ":=", "=", "+=", "-=", "*=":
 		if s.exp.typ == "-" && s.lit.typ != "int" {
-			fmt.Fprintf(w, "%s %s %s(%s) // %s\n", s.lit.nam, s.opr, s.lit.typ, s.exp.str, s.lit.typ)
+			fmt.Fprintf(w, "%s %s %s(%s) // %s\n", s.lit.tok, s.opr, s.lit.typ, s.exp, s.lit.typ)
 		} else {
-			fmt.Fprintf(w, "%s %s %s // %s\n", s.lit.nam, s.opr, s.exp.str, s.lit.typ)
+			fmt.Fprintf(w, "%s %s %s // %s\n", s.lit.tok, s.opr, s.exp, s.lit.typ)
 		}
 	case "_":
-		fmt.Fprintf(w, "_ = %s\n", s.lit.nam)
+		fmt.Fprintf(w, "_ = %s\n", s.lit.tok)
 	case "++", "--":
-		fmt.Fprintf(w, "%s%s\n", s.lit.nam, s.opr)
+		fmt.Fprintf(w, "%s%s\n", s.lit.tok, s.opr)
 	}
 }
 
